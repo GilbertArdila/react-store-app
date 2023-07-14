319e2af136100fc625ec27c9ecc76be98711ac5e
gilbert <gilbertardila@yahoo.es>
2023-07-14 16:28:46 -0500

    iniciando proyecto react store con vite-tailwindcss-react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-red-100'>
+        <h1> Hello word</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
