79b24b78f31e94835723b1e6b830ab3e1acdeb9a
gilbert <gilbertardila@yahoo.es>
2023-07-18 08:52:38 -0500

    crando páginas

---
+++ src/pages/App/index.jsx
@@ -0,0 +1,22 @@
+import Home from '../Home';
+import MyAccount from '../MyAccount';
+import MyOrder from '../MyOrder';
+import MyOrders from '../MyOrders';
+import NotFound from '../NotFound';
+import SignIn from '../SignIn';
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-red-100'>
+        <h1> Hello word</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
