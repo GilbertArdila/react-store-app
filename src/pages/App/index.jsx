df14acf373d826d3afdca14ea3c410926309fdd0
gilbert <gilbertardila@yahoo.es>
2023-07-18 09:03:47 -0500

    creando las rutas

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,22 +1,48 @@
-import Home from '../Home';
-import MyAccount from '../MyAccount';
-import MyOrder from '../MyOrder';
-import MyOrders from '../MyOrders';
-import NotFound from '../NotFound';
-import SignIn from '../SignIn';
-import './index.css'
+import { useRoutes, BrowserRouter } from "react-router-dom";
+
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+import "./index.css";
+
+const AppRoutes = () => {
+  return useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/my-account",
+      element: <MyAccount />,
+    },
+    {
+      path: "/my-orders",
+      element: <MyOrders />,
+    },
+    {
+      path: "/my-order",
+      element: <MyOrder />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+};
 
 function App() {
-  
-
   return (
-    <>
-      <div className='bg-red-100'>
-        <h1> Hello word</h1>
-      </div>
-      
-    </>
-  )
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
